06f8e1a5399ffff3de3e657833d0816c376cb9bf
Kacper Kwinta <kaspersky.kwinta@gmail.com>
2023-09-15 19:46:02 +0200

    hero section mobile ready && finish

--- js/menu.js
+++ js/menu.js
@@ -1,16 +1,30 @@
 "use strict";
 
+// Selecting elements from the DOM
 const hamburger = document.querySelector(".hamburger");
 const closeHamburger = document.querySelector(".close-hamburger");
 const navLinks = document.querySelector("nav > .wrapper");
-const navAfter = document.querySelector("nav::after");
+const nav = document.querySelector("nav");
+const menuBg = document.querySelector(".menu-bg");
 
+// Function to toggle the menu
 function toggleMenu() {
 	navLinks.classList.toggle("visible");
 	hamburger.classList.toggle("hidden");
 	closeHamburger.classList.toggle("hidden");
-	navAfter.style.opacity = 1;
+	menuBg.classList.toggle("visible");
 }
 
+// Event listener for hamburger icon click
 hamburger.addEventListener("click", toggleMenu);
+
+// Event listener for close hamburger icon click
 closeHamburger.addEventListener("click", toggleMenu);
+
+// Selecting all links in the menu
+const links = document.querySelectorAll("nav a");
+
+// Adding click event listener to each link
+links.forEach((link) => {
+	link.addEventListener("click", toggleMenu);
+});
